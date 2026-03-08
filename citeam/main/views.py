c959b0f95ee419d39d912062ec9dcaa1df237f99
Latypov Ramil <yazuk85@gmail.com>
2026-03-08 16:17:54 +0300

    выборка из БД с bootstrap

--- citeam/main/views.py
+++ citeam/main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
